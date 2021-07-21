e5c957e2dade618d65154c74dcdeb08da0c502d5
Gabriel Martins <gabriel.hamartins@gmail.com>
2021-07-21 00:44:53 -0300

    developed update by id person

--- src/main/java/one/digitalinnovation/personapi/service/PersonService.java
+++ src/main/java/one/digitalinnovation/personapi/service/PersonService.java
@@ -28,10 +28,7 @@ public class PersonService {
     public MessageResponseDTO createPerson(PersonDTO personDTO){
         Person personToSave = personMapper.toModel(personDTO);
         Person savedPerson = personRepository.save(personToSave);
-        return MessageResponseDTO
-                .builder()
-                .message("Created Person with ID: " + savedPerson.getId())
-                .build();
+        return createMessageResponde(savedPerson.getId(), "Created Person with ID: ");
     }
 
     public List<PersonDTO> listAll() {
@@ -51,8 +48,24 @@ public class PersonService {
         personRepository.deleteById(id);
     }
 
+    public MessageResponseDTO updateById(Long id, PersonDTO personDTO) throws PersonNotFoundException {
+        verifyIfExists(id);
+        Person personToUpdate = personMapper.toModel(personDTO);
+        Person updatedPerson = personRepository.save(personToUpdate);
+        return createMessageResponde(updatedPerson.getId(), "Updated Person with ID: ");
+    }
+
+    private MessageResponseDTO createMessageResponde(Long id, String message) {
+        return MessageResponseDTO
+                .builder()
+                .message(message + id)
+                .build();
+    }
+
     private Person verifyIfExists(Long id) throws PersonNotFoundException {
         return personRepository.findById(id)
                 .orElseThrow(() -> new PersonNotFoundException(id));
     }
+
+
 }
